1293a9315b16b6092ec51784fb577cffad0f9ffd
liukaixin <liukx@tedu.cn>
2019-04-02 10:34:55 +0800

    提交初始化branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,34 +8,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
 
-import { AnimationComponent } from './animation/animation.component';
-import { TestDynamicComponent } from './test-dynamic/test-dynamic.component';
-import { Child1Component } from './test-dynamic/child1/child1.component';
-import { TestShadowDomComponent } from './test-shadow-dom/test-shadow-dom.component';
-import { TestEmulatedModeComponent } from './test-emulated-mode/test-emulated-mode.component';
-import { TestNgContentComponent } from './test-ng-content/test-ng-content.component';
-import { ChildTwoComponent } from './test-ng-content/child-two/child-two.component';
-import { ChildThreeComponent } from './test-ng-content/child-three/child-three.component';
-import { TestContentChildComponent } from './test-content-child/test-content-child.component';
-import { ChildFourComponent } from './test-content-child/child-four/child-four.component';
-import { ChildFiveComponent } from './test-content-child/child-five/child-five.component';
-import { TestViewChildComponent } from './test-view-child/test-view-child.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    AnimationComponent,
-    TestDynamicComponent,
-    Child1Component,
-    TestShadowDomComponent,
-    TestEmulatedModeComponent,
-    TestNgContentComponent,
-    ChildTwoComponent,
-    ChildThreeComponent,
-    TestContentChildComponent,
-    ChildFourComponent,
-    ChildFiveComponent,
-    TestViewChildComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,7 +21,7 @@ import { TestViewChildComponent } from './test-view-child/test-view-child.compon
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [
-    Child1Component,
+    
   ]
 })
 export class AppModule { }
